59b730dac41d6d5ec4608515a0d498a3bb631dbd
klavs <klavs.kalnejs@gmail.com>
2021-06-16 14:32:40 +0300

    csi-camera

--- main.cpp
+++ main.cpp
@@ -25,11 +25,35 @@
         }                                                     \
     } while (0);
 
+std::string gstreamer_pipeline (int id, int mode, int capture_width, int capture_height, int display_width, int display_height, int framerate, int flip_method) {
+    return "nvarguscamerasrc sensor-id=(int)" + std::to_string(id) + " sensor-mode=(int)" + std::to_string(mode) + " ! video/x-raw(memory:NVMM), width=(int)" + std::to_string(capture_width) + ", height=(int)" +
+           std::to_string(capture_height) + ", format=(string)NV12, framerate=(fraction)" + std::to_string(framerate) +
+           "/1 ! nvvidconv flip-method=" + std::to_string(flip_method) + " ! video/x-raw, width=(int)" + std::to_string(display_width) + ", height=(int)" +
+           std::to_string(display_height) + ", format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink";
+}
+
 int main(int argc, char *argv[]){
+
+  int id = 0;
+  int mode = 3;
+  int capture_width = 1280 ;
+  int capture_height = 720 ;
+  int display_width = 1280 ;
+  int display_height = 720 ;
+  int framerate = 60 ;
+  int flip_method = 0 ;
+
+  std::string pipeline = gstreamer_pipeline(id, mode, capture_width,
+        capture_height,
+        display_width,
+        display_height,
+        framerate,
+        flip_method);
+  std::cout << "Using pipeline: \n\t" << pipeline << "\n";
 
   cv::VideoCapture camera;
 
-  camera.open(0);
+  camera.open(pipeline, cv::CAP_GSTREAMER);
 
   // check if we succeeded
   if (!camera.isOpened()) {
