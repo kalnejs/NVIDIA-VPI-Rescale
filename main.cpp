4bf31bbbbecd05207ce8a34ef0c26919c4747278
klavs <klavs.kalnejs@gmail.com>
2021-06-16 14:35:00 +0300

    csi-camera

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@
     } while (0);
 
 std::string gstreamer_pipeline (int id, int mode, int capture_width, int capture_height, int display_width, int display_height, int framerate, int flip_method) {
-    return "nvarguscamerasrc sensor-id=(int)" + std::to_string(id) + " sensor-mode=(int)" + std::to_string(mode) + " ! video/x-raw(memory:NVMM), width=(int)" + std::to_string(capture_width) + ", height=(int)" +
+    return "nvarguscamerasrc sensor-id=" + std::to_string(id) + " sensor-mode=" + std::to_string(mode) + " ! video/x-raw(memory:NVMM), width=(int)" + std::to_string(capture_width) + ", height=(int)" +
            std::to_string(capture_height) + ", format=(string)NV12, framerate=(fraction)" + std::to_string(framerate) +
            "/1 ! nvvidconv flip-method=" + std::to_string(flip_method) + " ! video/x-raw, width=(int)" + std::to_string(display_width) + ", height=(int)" +
            std::to_string(display_height) + ", format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink";
